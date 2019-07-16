69a7e4d274181b605f9230211a07edca658d5c06
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-07-16 07:11:06 +0000

    data backup for migration

--- req.sh
+++ req.sh
@@ -3,6 +3,6 @@ set -x
 c=$1
 num=$2
 let inp=c*50
-mkdir -p app_engine_results
-ab -n $inp -c $c https://build-run-go-multiple.appspot.com/fib/$num \
-> app_engine_results/$num-$inp-$c.txt
+mkdir -p cloud_run_results
+ab -n $inp -c $c https://build-run-go-multiple-uaa5i34bwq-uc.a.run.app/fib/$num \
+> cloud_run_results/$num-$inp-$c.txt
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in 20 25 30
 do
-	for j in 2 4 8 16 32 64 128 256 512
+	for j in 2 4 8 16 32 64 128 256 512 1024 2048
 	do
 		 ./req.sh $j $i
 	done
